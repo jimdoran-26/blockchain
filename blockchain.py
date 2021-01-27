b7863e8b6d357833fe58845f7a64e6eafac02f75
Jim Doran <jimdoran26@gmail.com>
2021-01-27 18:49:48 -0500

    changed to return blocks

--- blockchain.py
+++ blockchain.py
@@ -40,23 +40,21 @@ class Blockchain():
         self.chain = chain
 
     def add(self,block):
-        self.chain.append({
-            'hash':block.hash(),
-            'previous':block.previous_hash,
-            'number':block.number,
-            'data':block.data,
-            'nonce':block.nonce
-        })
+        self.chain.append(block)
+
+    def remove(self,block):
+        self.chain.remove(block)
+
 
     def mine(self,block):
         try:
-            block.previous_hash=self.chain[-1].get('hash')
+            block.previous_hash=self.chain[-1].hash()
         except IndexError:
             pass
 
         while True:
             if block.hash()[:self.difficulty] == "0" * self.difficulty:
-                self.add(block);break
+                self.add(block); break
             else:
                 block.nonce+=1
 
